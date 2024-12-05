93a6a1f0bc7a837ef4a07a3ecb0874b9f69c3580
shisa3110 <en5725do.0113@gmail.com>
2024-12-05 15:38:18 +0900

    deviseのビューファイルとマイグレーションファイルの生成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Set localhost to be used by links generated in mailer templates.
+  # deviseがメールを送る際のホスト名を指定してあげる設定
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # Print deprecation notices to the Rails logger.
