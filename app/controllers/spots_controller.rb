4a11790360308a6a9e425a0b188acb9f1ed428e5
shisa3110 <en5725do.0113@gmail.com>
2024-12-09 16:46:02 +0900

    [Update]データベースへのデータ格納＆マップへのピン立て表示

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -6,4 +6,20 @@ class SpotsController < ApplicationController
   def new
     @spot = Spot.new
   end
+  
+  def create
+    @spot = Spot.new(spot_params)
+    if @spot.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+
+  private
+  def spot_params
+    params.require(:spot).permit(:name, :post_code, :address, :phone_number, :web_site, :latitude, :longitude, :opening_hours)
+  end
+
 end
