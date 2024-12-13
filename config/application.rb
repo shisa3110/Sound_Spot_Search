1ed5549ed355dd8a86566496289e1724bf6f10b2
shisa3110 <en5725do.0113@gmail.com>
2024-12-14 03:52:25 +0900

    [Fix]deviseのフラッシュメッセージの日本語化修正

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,5 @@ module Myapp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
-
-        # localesフォルダ内の全てのymlファイルをロード
-        config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
