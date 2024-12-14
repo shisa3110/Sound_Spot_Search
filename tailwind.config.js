1b7aadcaf92f9d5c3d724cc9baaedf48d2a03ebe
shisa3110 <en5725do.0113@gmail.com>
2024-12-14 16:26:14 +0900

    [fix]全体的なviewの整理

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,23 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   theme: {
-    extend:{
-      fontFamily: {
-        kiwi: ['Kiwi Maru', 'serif']
-      }
-      
+    extend: {},
+    fontFamily: {
+      title: ['Sans'],
+      body:  ['Kiwi Maru', 'Sans'],
     }
   },
   plugins: [require("daisyui")],
   daisyui: {
     darkTheme: false, // ダークモードをONにする場合は削除
-    themes: ['retro']
+    themes: [
+      {
+        mytheme: {
+          ...require("daisyui/src/theming/themes")["retro"],
+          "base-200": "F3EBE1"
+        },
+      },
+      'retro'
+    ],
   },
 }
