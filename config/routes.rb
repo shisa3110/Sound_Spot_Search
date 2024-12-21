22c778dc2ced56394df040a6cd21ae012e68d3d1
shisa3110 <en5725do.0113@gmail.com>
2024-12-21 22:48:34 +0900

    [add]ユーザープロフィールページのMVC追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+}
+  resource :users, only: [:show] do
+    collection do
+      get 'profile', to: 'users#profile'
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "static_pages#top"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
