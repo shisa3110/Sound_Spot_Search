2bf7d0125a6f09593e89fcd571ba1d38300cf70a
shisa3110 <en5725do.0113@gmail.com>
2025-01-23 21:29:05 +0900

    [Fix]spotのカテゴリのenum記述の修正

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,5 +1,5 @@
 class Spot < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
 
-  #enum category: { studio:0, karaoke:1, public_facilities:2 }
+  enum :category, { studio:0, karaoke:1, public_facilities:2 }
 end
