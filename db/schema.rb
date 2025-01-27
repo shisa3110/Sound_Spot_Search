39318fa54394f0c5f04f6e5180d3f50a076b70d9
shisa3110 <en5725do.0113@gmail.com>
2025-01-28 00:07:40 +0900

    [Add]tagsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_23_110706) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_27_150154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -38,6 +38,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_23_110706) do
     t.datetime "updated_at", null: false
     t.string "place_id"
     t.string "opening_hours"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
