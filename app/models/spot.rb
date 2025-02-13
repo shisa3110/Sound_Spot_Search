d42f42331528eb66c0b2f4eb6c60cb5566fb81bd
shisa3110 <en5725do.0113@gmail.com>
2025-02-13 21:07:38 +0900

    [Add]spotモデルへのsave_tag,tag_namesメソッドの追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -5,4 +5,18 @@ class Spot < ApplicationRecord
   has_many :tags, through: :spot_tags
 
   enum :category, { studio:0, karaoke:1, public_facilities:2 }
+
+  def save_with_tags(tag_names:)
+    ActiveRecord::Base.transaction do
+      self.tags = tag_names.map { |name| Tag.find_or_initialize_by(name: name.strip) }
+      save!
+    end
+    true
+  rescue StandardError
+    false
+  end
+
+  def tag_names
+    tags.map(&:name).join(',')
+  end
 end
