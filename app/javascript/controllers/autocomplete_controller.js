4ebb595c096f16af5b9777e9b4be5846cfe3fbb0
shisa3110 <en5725do.0113@gmail.com>
2025-02-21 01:40:13 +0900

    [Add]jsのautocompleteコントローラの記述

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -4,4 +4,44 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
   }
+}import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="autocomplete"
+//Stimulusでは、クラス（ここではControllerを継承したクラス）を作ることで、ページ上の要素を動的に操作できます。
+export default class extends Controller {
+  // これは Stimulusの「Value API」 を使っていて、urlValue というプロパティを使えるようにしています。
+  static values = { url: String }
+  static targets = ["results"]
+  
+
+  search(event) {
+    const query = encodeURIComponent(event.target.value);  
+    const url = `${this.urlValue}?q=${query}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.updateResults(data);
+      })
+      .catch(error => console.error('Error fetching autocomplete data:', error));
+  }
+
+  updateResults(data) {
+    this.resultsTarget.innerHTML = '';
+
+    data.forEach(item => {
+      const li = document.createElement('li');
+      li.textContent = item.title;  
+      li.addEventListener('click', () => {
+        this.selectResult(item);
+      });
+      this.resultsTarget.appendChild(li);
+    });
+  }
+
+  selectResult(item) {
+    this.element.querySelector('input').value = item.name;  
+
+    this.resultsTarget.innerHTML = '';
+  }
 }
