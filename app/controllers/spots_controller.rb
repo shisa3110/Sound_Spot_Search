21481130f43fd8dfe21d8a31d2883dceb5497701
shisa3110 <en5725do.0113@gmail.com>
2025-02-22 19:44:34 +0900

    [Add]spotsコントローラindexアクションへ

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -11,7 +11,7 @@ class SpotsController < ApplicationController
 
   def index
     @q = Spot.ransack(params[:q])
-    @spots = @q.result(distinct: true)
+    @spots = @q.result(distinct: true).page(params[:page]).per(10)
   end
 
   def create
