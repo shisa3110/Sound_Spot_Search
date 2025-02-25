88fb08aaca73827d679e49aff9f1bcb0c92d5a07
shisa3110 <en5725do.0113@gmail.com>
2025-02-26 00:35:40 +0900

    [Add]口コミ機能実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,11 @@
 class ReviewsController < ApplicationController
   def create
-    review = Review.reviews.build(review_params)
-    if review.save
-      redirect_to spot_path(review.spot), success: "口コミを投稿しました"
+    @spot = Spot.find(params[:spot_id]) 
+    @review = @spot.reviews.build(review_params) 
+    if @review.save
+      redirect_to spot_path(reviews.spot), success: "口コミを投稿しました"
     else
-      redirect_to spot_path(review.spot), danger: "口コミを投稿できませんでした"
+      redirect_to spot_path(@review.spot), danger: "口コミを投稿できませんでした"
     end
   end
   
