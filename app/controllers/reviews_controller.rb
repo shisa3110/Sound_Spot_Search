fd35bfa01d751cb215263128c9e388e3e4b9604b
shisa3110 <en5725do.0113@gmail.com>
2025-02-27 00:11:32 +0900

    [Fix]口コミ投稿機能の修正、ビュー表示調整

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,7 @@ class ReviewsController < ApplicationController
     @spot = Spot.find(params[:spot_id]) 
     @review = @spot.reviews.build(review_params) 
     if @review.save
-      redirect_to spot_path(reviews.spot), success: "口コミを投稿しました"
+      redirect_to spot_path(@review.spot), success: "口コミを投稿しました"
     else
       redirect_to spot_path(@review.spot), danger: "口コミを投稿できませんでした"
     end
@@ -12,6 +12,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:body).merge(spot_id: params[:spot_id])
+    params.require(:review).permit(:body).merge(user_id: current_user.id)
   end
 end
