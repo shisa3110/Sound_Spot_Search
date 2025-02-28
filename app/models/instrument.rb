6b231a6e37507b50640790e0120ca33f7f7183b0
shisa3110 <en5725do.0113@gmail.com>
2025-02-28 23:51:49 +0900

    [fix]楽器の種類kindのenum設定とi18n化

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -5,4 +5,11 @@ class Instrument < ApplicationRecord
   belongs_to :user
 
   mount_uploader :instrument_image, InstrumentImageUploader
+
+  enum :kind, { not_selected: 0, violin: 1, viola: 2, cello: 3, contra_bass: 4, acoustic_guitar: 5, 
+                electric_guitar: 6, electric_bass: 7, piano: 8, keyboard: 9, flute: 10,
+                piccolo: 11, clarinet: 12, saxophone: 13, oboe: 14, english_horn: 15, 
+                fagott: 16, trumpet: 17, trombone: 18, horn: 19, euphonium:20,
+                tuba: 21, drum: 22, keyboard_percussion: 23, other_percussion: 24, other_strings: 25,
+                other_wind_instrument: 26, japanese_instrument: 27}
 end
