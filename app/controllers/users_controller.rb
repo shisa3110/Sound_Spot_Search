981bda88080bcee1543dfa980e53194fab4f067a
shisa3110 <en5725do.0113@gmail.com>
2025-03-02 01:10:26 +0900

    [Add]my_reviewsとmy_instrumentsのアクションをuserコントローラに設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def profile
     @user = current_user
   end
+
+  def my_reviews
+    @my_reviews = current_user.reviews.includes(:spot)
+  end
+  
+  def my_instruments
+    @my_instruments = current_user.instruments.includes(:user)
+  end
 end
