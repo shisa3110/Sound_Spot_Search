514ada69205ca69b08aea6eefc19699d8bf5c809
shisa3110 <en5725do.0113@gmail.com>
2025-03-02 12:14:49 +0900

    [Add]利用規約とプライバシーポリシーのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,15 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "static_pages#top"
+  
+  get 'terms_of_use', to: 'static_pages#terms_of_use'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  
   # 地図検索ページのルーティング
   get "spots/map"
   resources :spots do
