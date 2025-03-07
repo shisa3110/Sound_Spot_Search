a944d607768384e9d09fe398433b4838b6a0fea2
shisa3110 <en5725do.0113@gmail.com>
2025-03-08 03:09:57 +0900

    [Fix]Google認証不具合解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :authentications, dependent: :destroy
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
