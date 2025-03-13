ed59f21d018cf80734181b3c5941f7588f4f2749
shisa3110 <en5725do.0113@gmail.com>
2025-03-13 22:18:50 +0900

    [Add]letter_openerのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -50,4 +50,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
