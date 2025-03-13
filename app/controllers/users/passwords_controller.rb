4628e067fe6045aef9bbb0ad7ab9895d7a579f57
shisa3110 <en5725do.0113@gmail.com>
2025-03-14 00:20:04 +0900

    [FIx]パスワードリセットメール送信設定、文章設定、画面ビュー調整

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+  
+    if successfully_sent?(resource)
+      redirect_to after_sending_reset_password_instructions_path_for(resource_name)
+    else
+      flash[:alert] = "メールアドレスを確認してください" if is_navigational_format?
+      render :new 
+    end
+  end
   # GET /resource/password/new
   # def new
   #   super
