30a29cd890b1db729a03449dd8778b49eea82c28
shisa3110 <en5725do.0113@gmail.com>
2025-03-16 16:03:01 +0900

    [Fix]capybaraの設定修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,8 @@
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-  driven_by :selenium, using: :headless_chrome #←ブラウザの表示無
-  #driven_by :selenium_chrome　←ブラウザの表示有
-  end
+Capybara.register_driver :remote_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('no-sandbox')
+  options.add_argument('headless')
+  options.add_argument('disable-gpu')
+  options.add_argument('window-size=950,800')
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: ENV['SELENIUM_DRIVER_URL'], capabilities: options)
 end
