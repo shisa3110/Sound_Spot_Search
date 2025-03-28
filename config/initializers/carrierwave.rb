83f86d582acd6f29845467aefd2c5a573fba0ff4
shisa3110 <en5725do.0113@gmail.com>
2025-03-29 02:05:32 +0900

    [Fix]config/initializers/carrierwave.rbの修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,8 +12,8 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      use_iam_profile: true,
       region: ENV['S3_REGION'],
-      # path_style: true
     }
   else
     config.storage :file
