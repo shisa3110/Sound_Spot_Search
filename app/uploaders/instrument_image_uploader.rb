61a995f478844985a30925c0e31621d79853f844
shisa3110 <en5725do.0113@gmail.com>
2025-03-29 22:16:52 +0900

    [Fix]uploaderの修正

--- app/uploaders/instrument_image_uploader.rb
+++ app/uploaders/instrument_image_uploader.rb
@@ -3,7 +3,8 @@ class InstrumentImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
   # include CarrierWave::Vips
-
+  
+  # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
     storage :fog
   else
@@ -13,9 +14,6 @@ class InstrumentImageUploader < CarrierWave::Uploader::Base
   def default_url
     "instrument_default.png"
   end
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,3 @@
-require 'fog/aws'
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
@@ -12,7 +11,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-      region: ENV['S3_REGION'],
+      region: ENV['S3_REGION']
     }
   else
     config.storage :file
