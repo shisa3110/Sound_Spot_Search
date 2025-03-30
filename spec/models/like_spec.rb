85728e79453bc01cf0d8d967dc37e1810d92cd6b
shisa3110 <en5725do.0113@gmail.com>
2025-03-30 16:42:06 +0900

    [Add]likeモデルスペックのテストコード追加

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    it '設定したすべてのバリデーションが機能しているか' do
+      like = build(:like)
+      expect(like).to be_valid
+      expect(like.errors).to be_empty
+    end
+
+    it '重複したuser_idが登録された場合、バリデーションが機能してinvalidエラーになるか' do
+      like = create(:like)
+      like_with_duplicated = build(:like, user_id: like.user_id)
+      expect(like_with_duplicated_user_id).to be_invalid
+    end
+
+    it 'user_idが重複していない場合、バリデーションエラーが起きないか'
+      like = create(:like)
+      like_with_another = create(:like, user_id: like.user_id + 1)
+      expect(like_with_another_user_id).to be_valid
+    end
+  end
 end
