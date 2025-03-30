dfcfe33d6d23325b551f6335c2c6a92dd94e8d09
shisa3110 <en5725do.0113@gmail.com>
2025-03-30 17:48:58 +0900

    [Add]spot_tagモデルスペックのテストコード記述追加

--- spec/models/spot_tag_spec.rb
+++ spec/models/spot_tag_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe SpotTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    it '設定したすべてのバリデーションが機能しているか' do
+      spot_tag = build(:spot_tag)
+      expect(spot).to be_valid
+      expect(spot.errors).to be_empty
+    end
+
+    it 'spot_idがない場合、バリデーションが機能してinvalidエラーになるか' do
+      without_spot_id = build(:spot_tag, spot_id: nil)
+      expect(swithout_spot_id).to be_invalid
+    end
+
+    it 'user_idがない場合、バリデーションが機能してinvalidエラーになるか' do
+      without_user_id = build(:spot_tag, user_id: nil)
+      expect(swithout_user_id).to be_invalid
+    end
+  end
 end
