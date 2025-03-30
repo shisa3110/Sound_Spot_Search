1d28d0df1b163314ea4d38f30276ea15de942994
shisa3110 <en5725do.0113@gmail.com>
2025-03-30 17:13:34 +0900

    [Add]instrumentモデルスペックへのテストコード記述

--- spec/models/instrument_spec.rb
+++ spec/models/instrument_spec.rb
@@ -1,24 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Instrument, type: :model do
-    let(:instrument) { create(:instrument) }
+  describe 'バリデーションチェック' do
+    it '設定したすべてのバリデーションが機能しているか' do
+      instrument = build(:instrument)
+        expect(instrument).to be_valid
+        expect(instrument.errors).to be_empty
+      end
 
-    it "有効な楽器情報を持つ場合、バリデーションを通過する" do
-      expect(instrument).to be_valid
+    it "titleがない場合、バリデーションが機能してinvalidになるか" do
+      instrument_without_title = build(:instrument, title: nil)
+      expect(instrument_without_title).to be_invalid
+      expect(instrument_without_title.errors[:title]).to eq ["投稿に失敗しました"]
     end
 
-    it "タイトルがない場合、無効である" do
-      instrument.title = nil
-      expect(instrument).not_to be_valid
+    it "commentがない場合、バリデーションが機能してinvalidになるか" do
+      instrument_without_comment = build(:instrument, comment: nil)
+      expect(instrument_without_comment).to be_invalid
+      expect(instrument_without_comment.errors[:comment]).to eq ["投稿に失敗しました"]
     end
 
-    it "コメントがない場合、無効である" do
-      instrument.comment = nil
-      expect(instrument).not_to be_valid
+    it "instrument_imageがない場合、バリデーションが機能してinvalidになるか" do
+      instrument_without_image = build(:instrument, instrument_image: nil)
+      expect(instrument_without_image).to be_invalid
+      expect(instrument_without_image[:instrument_image]).to eq ["投稿に失敗しました"]
     end
 
-    it "画像がない場合、無効である" do
-      instrument.instrument_image = nil
-      expect(instrument).not_to be_valid
+    it "titleの文字数が120文字より多い場合、バリデーションが機能してinvalidになるか"
+      instrument = build(:instrument, title: Faker::Lorem.characters(number: 121))
+      expect(instrument).to be_invalid
+      expect(instrument[:title]).to eq ["投稿に失敗しました"]
+    end
+
+    it "commentが65,535文字より多い場合、バリデーションが機能してinvalidになるか"
+      instrument = build(:instrument, comment: Faker::Lorem.characters(number: 65_535))
+      expect(instrument).to be_invalid
+      expect(instrument[:title]).to eq ["投稿に失敗しました"]
     end
   end
+end
