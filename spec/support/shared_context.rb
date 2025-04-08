bfab731aca83ffaefb856cf4851545133198e1f9
shisa3110 <en5725do.0113@gmail.com>
2025-04-08 19:59:01 +0900

    [Fix]system spec(spot)修正

--- spec/support/shared_context.rb
+++ spec/support/shared_context.rb
@@ -1,10 +1,8 @@
-RSpec.shared_context 'with_sign_in' do
-  let!(:user) { FactoryBot.create(:user, email: 'test@example.com', password: 'password') }
-  
+RSpec.shared_context 'with_sign_in' do  
   before do
     visit new_user_session_path
-    fill_in 'メールアドレス', with: user.email
-    fill_in 'パスワード', with: 'password'
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
     click_button 'ログイン'
   end  
 end
