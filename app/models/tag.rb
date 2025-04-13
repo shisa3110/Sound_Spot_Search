79e3a80de3f8af2b90a29fa0b5a19a31ba5f9a64
shisa3110 <en5725do.0113@gmail.com>
2025-04-13 11:16:24 +0900

    [Delete]不要なコメント削除

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
   has_many :spot_tags, dependent: :destroy
   has_many :spots, through: :spot_tags
-  # tagには必ずnameをつけ、他のものと被らない。
+
   validates :name, presence: true, uniqueness: true
 
   def self.ransackable_attributes(auth_object = nil)
