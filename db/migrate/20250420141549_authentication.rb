b0674945ee822a6bf15edb33fe4889aeecfcf38e
shisa3110 <en5725do.0113@gmail.com>
2025-04-20 23:17:46 +0900

    [Delete]不要なauthenticationsテーブルの削除

---
+++ db/migrate/20250420141549_authentication.rb
@@ -0,0 +1,5 @@
+class Authentication < ActiveRecord::Migration[8.0]
+  def change
+    drop_table :authentications
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_16_072751) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_20_141549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-
-  create_table "authentications", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_authentications_on_user_id"
-  end
 
   create_table "bookmarks", force: :cascade do |t|
     t.bigint "user_id"
@@ -117,7 +108,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_16_072751) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "authentications", "users"
   add_foreign_key "bookmarks", "spots"
   add_foreign_key "bookmarks", "users"
   add_foreign_key "instruments", "users"
